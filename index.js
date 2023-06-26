ee3a9dcf8f5675d480c6b769c81e75e87a28156d
ANANTHA KUMAR.S <paulananthooo7@gmail.com>
2023-06-26 16:58:08 +0530

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".typing-text", {
+    strings: ["NetSuite Consultation", "Sharepoint Consultation", "software Consultation",],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 30,
+    backDelay: 500,
+});
